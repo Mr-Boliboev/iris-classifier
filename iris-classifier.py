b416273591388bb15396fe3271842f945ab67b5d
Sukhrob <suxrobboliyev1@gmail.com>
2025-07-13 11:36:00 +0900

    Vizualizatsiyalar qushildi

--- iris-classifier.py
+++ iris-classifier.py
@@ -2,7 +2,12 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import classification_report, confusion_matrix
 # Ma'lumotlarni yuklash
 iris = load_iris()
 X = iris.data
@@ -18,5 +23,18 @@ model.fit(X_train, y_train)
 # Bashorat va aniqlikni baholash
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target
 
+sns.pairplot(df, hue='target')
+plt.savefig('images/pairplot.png')  # Saqlab qo‘yish
+plt.show()
 print(f"Model aniqligi: {accuracy * 100:.2f}%")
+
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
+
+sns.heatmap(confusion_matrix(y_test, predictions), annot=True, cmap="Blues")
+plt.title("Confusion Matrix")
+plt.show()
